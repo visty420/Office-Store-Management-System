ad1d0a3099d5457341391d4a6efbae58182d1d23
visty <117592958+visty420@users.noreply.github.com>
2023-08-27 16:11:10 +0300

    Initial commit
    Created the project

---
+++ BookstoreManagementPlatform/src/main/java/com/example/bookstoremanagementplatform/BookstoreManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstoremanagementplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreManagementPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreManagementPlatformApplication.class, args);
+    }
+
+}
