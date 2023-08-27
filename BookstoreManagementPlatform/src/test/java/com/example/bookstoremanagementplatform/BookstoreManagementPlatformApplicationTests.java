a32f5fb61d5f974d63e3f87be15cd92e568125d9
visty <117592958+visty420@users.noreply.github.com>
2023-08-27 22:37:57 +0300

    Added app context unitest
    This test makes sure that the app loads correctly and checks if the dependencies and components are configured correctly.

--- BookstoreManagementPlatform/src/test/java/com/example/bookstoremanagementplatform/BookstoreManagementPlatformApplicationTests.java
+++ BookstoreManagementPlatform/src/test/java/com/example/bookstoremanagementplatform/BookstoreManagementPlatformApplicationTests.java
@@ -1,13 +1,19 @@
-package com.example.bookstoremanagementplatform;
+package com.example.bookstoremanagementplatform; // Asigurați-vă că pachetul este corect
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(classes = BookstoreManagementPlatformApplication.class)
 class BookstoreManagementPlatformApplicationTests {
+
+    @Autowired
+    private BookstoreManagementPlatformApplication bookstoreManagementPlatformApplication;
 
     @Test
     void contextLoads() {
+        assertNotNull(bookstoreManagementPlatformApplication);
     }
-
 }
